84c9048e08e99c2a4fec507df99e0a7176e915e1
rafaellfpc <rafaell@fleetprofitcenter.com>
2023-12-12 18:47:24 -0600

    Started to solve Day3-1 using C.

--- Day3-1/Main.hx
+++ Day3-1/Main.hx
@@ -2,12 +2,20 @@ class Main {
   static public function main():Void {
 #if sys
     trace("File system can be accessed");
+    final lineLength = 140;
 
     var fileInput:sys.io.FileInput = sys.io.File.read('data/input.txt');
+    var numbers = [];
+    var specialCharacters = [];
+    var currentNumber = '';
+    var numberRegexp = ~//
     while (true) {
       try {
         var char:String = fileInput.readString(1);
-        trace(char);
+        var newLinesRegExp = ~/\n|\r/;
+        if(!newLinesRegExp.match(char)) {
+          trace(char);
+        }
       } catch(errorReadingString:haxe.io.Eof) {
         break;
       }
---
+++ Day3-1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *inputFile;
+int data;
+
+int main() {
+  inputFile = fopen("./data/input.txt", "r");
+
+  return 0;
+}
+
+
